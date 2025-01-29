15f758e812965c923b96bf05e5b464d5c41b65b0
pavel99 <ght12345ght12345@gmail.com>
2025-01-29 17:27:27 +1000

    рабочий npm run watch

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,11 @@
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .vue()
-    .sourceMaps();
+    .sourceMaps()
+    .webpackConfig({
+        watchOptions: {
+            poll: 1000,
+            ignored: /node_modules/,
+        }
+    });
