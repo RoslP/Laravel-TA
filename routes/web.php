a64992dccf9e00a351faa791889771240fbceb5f
pavel99 <ght12345ght12345@gmail.com>
2025-01-30 20:21:36 +1000

    Getting users permissions.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\Api;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,13 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::middleware(['auth'])->group(function () {
-    Route::get('/get-products', [App\Http\Controllers\Auth\Api\ProductController::class, 'getList']);
+    Route::get('/get-products', [Api\ProductController::class, 'getList']);
+    Route::get('/get-permission', [Api\UserPermissionController::class, 'checkPermission']);
 });
